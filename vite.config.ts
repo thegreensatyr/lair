e631a9e3b662531da75dccec2bdebb0cf0d63515
thegreensatyr <thegreensatyr@gmail.com>
2026-06-08 22:56:05 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.',
+  build: {
+    rollupOptions: {
+      input: './index.html',
+    },
+  },
+  resolve: {
+    alias: {
+      '/src/main.tsx': './main.tsx',
+    },
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
